34d7c08b910b611beb6c25ddbcec2e8570d84671
Martian1648 <Martian1648@outlook.com>
2024-04-08 13:54:50 -0500

    make knight

--- content/entities/heroes.h
+++ content/entities/heroes.h
@@ -1,5 +1,9 @@
 #pragma once
-
+#include <memory>
+#include <entity.h>
 namespace Heroes {
-
+    void make_knight(std::shared_ptr<Entity>& hero){
+        hero->set_sprite("knight");
+        hero->set_max_health(10);
+    }
 }
